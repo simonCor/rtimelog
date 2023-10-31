c53b09985fd0c2ab70d8f17429b267af3c4edcf4
Simon Schwarz <simon.schwarz@fobg.de>
2023-10-31 21:29:30 +0100

    caotic development

--- src/cli.rs
+++ src/cli.rs
@@ -2,6 +2,30 @@ use clap::Parser;
 use chrono::{Local, NaiveDateTime, Timelike, Weekday};
 use colored::*;
 use crate::parser;
+use serde_derive::{Serialize, Deserialize};
+use home;
+
+fn get_timelog_file_path() -> std::path::PathBuf
+{
+    match home::home_dir() {
+        Some(mut path) => {
+            path.push(".gtimelog/timelog.txt");
+            path
+        },
+        None => {
+            panic!("Impossible to get your home dir!");
+        },
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+struct CliConfig {
+    timelog_file: std::path::PathBuf,
+}
+
+impl ::std::default::Default for CliConfig {
+    fn default() -> Self { Self { timelog_file: get_timelog_file_path() } }
+}
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -20,14 +44,31 @@ struct Args {
     new_entry: String,
 }
 
+fn print_entry(entry: (NaiveDateTime, (String, Vec<String>)))
+{
+    print!("{} - ", entry.0.to_string().green());
+    print!("{} -- ", entry.1.0);
+    for tag in entry.1.1 {
+        print!("{} ", tag.cyan())
+    }
+    print!("\n");
+}
+
 pub fn cli()
 {
     let args = Args::parse();
 
+    let config: CliConfig = match confy::load("rtimelog", None) {
+        Ok(config) => {config},
+        Err(_) => {panic!("config error");},
+    };
+
+    let parser = parser::Parser{path: get_timelog_file_path()}; //TODO: from config
+
     if args.new_entry.len() != 0
     {
         let local: NaiveDateTime = Local::now().naive_local();
-        parser::append_entry_to_file(local, args.new_entry);
+        parser.append_entry_to_file(local, args.new_entry);
         println!("Added entry for today")
     }
 
@@ -36,13 +77,13 @@ pub fn cli()
         let local: NaiveDateTime = Local::now().naive_local();
         let from = local.with_hour(0).expect("arggghh1").with_minute(0).expect("dfsdfsd");
         let to = local.with_hour(23).expect("arggghh2").with_minute(59).expect("sdfds");
-        let content = parser::get_range(from, to);
-        
+        let content = parser.get_range(from, to);
+
         //TODO: Print this prettier
         println!("Entries for today {}", local.format("%Y-%m-%d").to_string());
         for one_entry in content
         {
-            println!("{} - {}", one_entry.0, one_entry.1)
+            print_entry(one_entry);
         }
     }
 
@@ -53,12 +94,12 @@ pub fn cli()
 
         let from = week.first_day().and_hms_opt(0, 0, 0).unwrap();
         let to = week.last_day().and_hms_opt(23, 59, 59).unwrap();
-        let content = parser::get_range(from, to);
+        let content = parser.get_range(from, to);
         //TODO: Print this prettier
         println!("Entries for this week {} to {}", from.format("%Y-%m-%d").to_string().yellow(), to.format("%Y-%m-%d").to_string().yellow());
         for one_entry in content
         {
-            println!("{} - {}", one_entry.0.to_string().green(), one_entry.1)
+            print_entry(one_entry);
         }
     }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,57 +1,71 @@
 use std::fs::{OpenOptions, read_to_string};
 use std::io::prelude::*;
+use std::path::PathBuf;
 use chrono::{NaiveDateTime};
 use std::collections::BTreeMap;
-use home;
 
-fn get_timelog_file_path() -> std::path::PathBuf
-{
-    match home::home_dir() {
-        Some(mut path) => {
-            path.push(".gtimelog/timelog.txt");
-            path
-        },
-        None => {
-            panic!("Impossible to get your home dir!");
-        },
+pub struct Parser{
+    pub path: PathBuf,
+}
+
+impl Parser {
+    pub fn append_entry_to_file(&self, date: NaiveDateTime, message: String) {
+        let path = &self.path;
+
+        let mut output = OpenOptions::new()
+            .write(true)
+            .append(true)
+            .create(true)
+            .open(path)
+            .unwrap();
+
+            if let Err(e) = writeln!(output, "{}: {}", date.format("%Y-%m-%d %H:%M"), message) {
+                eprintln!("Couldn't write to file: {}", e);
+            }
+    }
+
+    pub fn get_range(&self, from: NaiveDateTime, to: NaiveDateTime) -> BTreeMap<NaiveDateTime, (String, Vec<String>)>{
+        let mut result: BTreeMap<NaiveDateTime, (String, Vec<String>)> = BTreeMap::new();
+        let path = &self.path;
+
+        for line in read_to_string(path).unwrap().lines() {
+            if !line.starts_with("#") {
+                let mut  splitted_line = line.splitn(3, ':');
+
+                let mut date = String::new();
+                date.push_str(splitted_line.next().expect("No content!"));
+                date.push(':');
+                date.push_str(splitted_line.next().expect("No content!"));
+                let descriptionWithTags: String = splitted_line.next().expect("No content!").to_string();
+                //TODO: Implement tags parsing (--)
+                let mut splitted_description_tags = descriptionWithTags.split("--");
+                let description: String = splitted_description_tags.next().expect("No description").to_string();
+                let tags = match splitted_description_tags.next() {
+                    Some(tagStrings) => {
+                        let mut tags: Vec<String> = Vec::new();
+                        for tag in tagStrings.split(" ")
+                        {
+                            tags.push(tag.to_string());
+                        }
+                        tags
+                    }
+                    None => {
+                        let tags: Vec<String> = Vec::new();
+                        tags
+                    }
+                };
+
+                let date = NaiveDateTime::parse_from_str(&date, "%Y-%m-%d %H:%M").expect("Parsing failed!");
+
+                if (date >= from) && (date <= to)
+                {
+                    result.insert(date, (description, tags));
+                }
+            }
+        }
+
+        result
     }
 }
 
-pub fn append_entry_to_file(date: NaiveDateTime, message: String) {
-    let path = get_timelog_file_path();
 
-    let mut output = OpenOptions::new()
-        .write(true)
-        .append(true)
-        .create(true)
-        .open(path)
-        .unwrap();
-
-        if let Err(e) = writeln!(output, "{}: {}", date.format("%Y-%m-%d %H:%M"), message) {
-            eprintln!("Couldn't write to file: {}", e);
-        }
-}
-
-pub fn get_range(from: NaiveDateTime, to: NaiveDateTime) -> BTreeMap<NaiveDateTime, String>{
-    let mut result: BTreeMap<NaiveDateTime, String> = BTreeMap::new();
-    let path  = get_timelog_file_path();
-
-    for line in read_to_string(path).unwrap().lines() {
-        let mut  splitted_line = line.splitn(3, ':');
-
-        let mut date = String::new();
-        date.push_str(splitted_line.next().expect("No content!"));
-        date.push(':');
-        date.push_str(splitted_line.next().expect("No content!"));
-        let description: String = splitted_line.next().expect("No content!").to_string();
-
-        let date = NaiveDateTime::parse_from_str(&date, "%Y-%m-%d %H:%M").expect("Parsing failed!");
-
-        if (date >= from) && (date <= to)
-        {
-            result.insert(date, description);
-        }
-    }
-
-    result
-}
